ba4a70d74d4c3d1b15e0adf1d6ae169ac8a29af7
Herson Guerrero Huh <83830469+herson-xyz@users.noreply.github.com>
2024-02-04 18:33:50 -0500

    Draw a circle at every click, over mouse position

--- Intro-to-Machine-Learning/linear-regression-ordinary-least-squares/sketch.js
+++ Intro-to-Machine-Learning/linear-regression-ordinary-least-squares/sketch.js
@@ -1,8 +1,26 @@
+var data = [];
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+	background(51);
+}
+
+function mousePressed(){
+	var x = map(mouseX, 0, width, 0, 1);
+	var y = map(mouseY, 0, height, 1, 0);
+
+	var point = createVector(x, y);
+	data.push(point);
 }
 
 function draw() {
-	// put drawing code here
+	background(51);
+
+	for (var i = 0; i < data.length; i++){
+		var x = map(data[i].x, 0, 1, 0, width);
+		var y = map(data[i].y, 0, 1, height, 0);
+		fill(255);
+		stroke(255);
+		ellipse(x, y, 8, 8);
+	}
 }
